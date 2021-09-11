d0a617cd24c08e914c96c17102107dce4994b339
Kalschne <you@example.comtiagoklschn026@gmail.com>
2021-09-11 08:10:28 -0300

    first commit

---
+++ App.Persistence/Migrations/20210910002535_nomeMigratio.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Persistence.Migrations
+{
+    public partial class nomeMigratio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    Peso = table.Column<int>(type: "integer", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ocorrencia",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PessoaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DataOcorrencia = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DataBaixaOcorrencia = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    Observacao = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ocorrencia", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ocorrencia_Pessoa_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ocorrencia_PessoaId",
+                table: "Ocorrencia",
+                column: "PessoaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ocorrencia");
+
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+        }
+    }
+}
